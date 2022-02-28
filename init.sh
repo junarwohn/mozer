9cd03a07a735ac831016e6ff5aa6985877e2be86
Joonho Wohn <givenarc@gmail.com>
2022-02-28 10:57:13 +0900

    rearranging structure

--- init.sh
+++ init.sh
@@ -7,5 +7,7 @@ else
         echo "version" $version "compatible!"  
         cmd="cp res/tvm/* ${TVM_HOME}"
         echo $cmd
+    else
+        echo "version" $version "is not compatible. Currently, only 0.8.0 is available"
     fi 
 fi
---
+++ python/mozer/slicer/analysis/analysis.py
@@ -0,0 +1,8 @@
+from tvm.contrib import graph_executor
+import numpy as np
+import os
+import json
+import pygraphviz as pgv
+
+def graph_analyze():
+    return 0
